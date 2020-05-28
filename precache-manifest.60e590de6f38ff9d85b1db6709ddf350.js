3ed9bcf290865a6869f09101788c08c0cc73b414
Elsa Desta <elsamenelik@gmail.com>
2020-05-28 13:16:37 +0200

    Updates

---
+++ precache-manifest.60e590de6f38ff9d85b1db6709ddf350.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3586a264a8d007222816d3feb6b594",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f2b621414b8a0187f59",
+    "url": "/static/css/main.713083bb.chunk.css"
+  },
+  {
+    "revision": "9dc4055474cfc60a3fe1",
+    "url": "/static/js/2.65267a5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.65267a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2b621414b8a0187f59",
+    "url": "/static/js/main.52aa9db3.chunk.js"
+  },
+  {
+    "revision": "eed54d528331316f7f49",
+    "url": "/static/js/runtime-main.a0d2aaf8.js"
+  },
+  {
+    "revision": "b8bed3d29ad9959747bea0ee74ec5827",
+    "url": "/static/media/ad.b8bed3d2.svg"
+  },
+  {
+    "revision": "009d6c00351f1f83707fd51ff4f7e4aa",
+    "url": "/static/media/ae.009d6c00.svg"
+  },
+  {
+    "revision": "ed49da9519f1a0a7b9c35a02457ba418",
+    "url": "/static/media/af.ed49da95.svg"
+  },
+  {
+    "revision": "8d8cb95182bf106266591db84f2d1f85",
+    "url": "/static/media/ag.8d8cb951.svg"
+  },
+  {
+    "revision": "6f9dbe734b08437e612042fd3e6d96c6",
+    "url": "/static/media/ai.6f9dbe73.svg"
+  },
+  {
+    "revision": "daf0c8372ef3354646a220ae5160babe",
+    "url": "/static/media/al.daf0c837.svg"
+  },
+  {
+    "revision": "54c311c6f2f5e501452e51534676ae21",
+    "url": "/static/media/am.54c311c6.svg"
+  },
+  {
+    "revision": "1b9b5f581173c1ce717361960afdc6dc",
+    "url": "/static/media/ao.1b9b5f58.svg"
+  },
+  {
+    "revision": "f66c042c51d54bae0f1f6f1e52b75658",
+    "url": "/static/media/play.f66c042c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flags-quizz/precache-manifest.5d0991cb1f9eca12e4190d1e95300cc9.js"
+  "/precache-manifest.60e590de6f38ff9d85b1db6709ddf350.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flags-quizz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
