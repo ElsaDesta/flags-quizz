c05fc05cae2c5ecf37c43b7cc4f54a36589554c9
Elsa Desta <elsamenelik@gmail.com>
2020-05-28 08:01:53 +0200

    Updates

--- flags/src/action/types.js
+++
@@ -1,2 +0,0 @@
-export const ADD_ANSWER = 'ADD_ANSWER';
-export const RESET_STORE = 'RESET_STORE';
--- flags/src/reducer/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux";
-import answerReducer from "./answerReducer";
-
-export default combineReducers({
-    answer: answerReducer
-});
