4a3511b0684aa9c25e15abd232685a60536f471d
Elsa Desta <elsamenelik@gmail.com>
2020-07-15 15:06:25 +0200

    added function to get 3 random countries

--- src/helpers/getRandomCountry.js
+++ src/helpers/getRandomCountry.js
@@ -10,13 +10,28 @@ const allCountries = {
 /*write a function that generates a random country*/
 
 function getRandomCountry(countryData) {
-  let arrayLength = Object.values(countryData).length;
-  let randomCountry = Object.values(countryData)[
+  const arrayLength = Object.values(countryData).length;
+  const randomCountry = Object.values(countryData)[
     Math.floor(Math.random() * arrayLength)
   ];
-  console.log(randomCountry);
-
   return randomCountry;
 }
 
-console.log(getRandomCountry(allCountries));
+/* get three random countries from data */
+
+function getMultiCountries(countryData) {
+  let arrayLength = Object.values(countryData).length;
+  let A = Object.values(countryData)[
+    Math.floor(Math.random() * arrayLength)
+  ].toString();
+  let B = Object.values(countryData)[
+    Math.floor(Math.random() * arrayLength)
+  ].toString();
+  let C = Object.values(countryData)[
+    Math.floor(Math.random() * arrayLength)
+  ].toString();
+  if (A === B || A === C || B === C) {
+    return getMultiCountries(countryData);
+  }
+  return [A, B, C];
+}
