621f49dbf585ed2cef7ac3409364fce184fdfa3e
Elsa Desta <elsamenelik@gmail.com>
2020-07-28 15:48:27 +0200

    first test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./StyleSheets/global.scss";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import Trivia2 from "./pages/trivia2";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
 import TriviaScreen from "./pages/TriviaScreen";
@@ -10,14 +9,14 @@ export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="app-wrapper">
+        <div className="App" data-test="app">
           <Route path="/">
             <Navbar  />
           </Route>
-
           <Switch>
-            <Route path="/trivia" component={Trivia2} />
-            <Route path="/triviascreen" component={TriviaScreen} />
+            <Route path="/triviascreen">
+              <TriviaScreen />
+            </Route>
             <Route path="/">
               <Home />
             </Route>
